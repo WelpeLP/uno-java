d1ae9f679a7e62031a3118167a48fa674cbe2333
bstege <benedikt.stege@gmail.com>
2021-04-14 12:12:12 +0200

    Werte für Karten festgelegt.

--- src/Datenelement.java
+++ src/Datenelement.java
@@ -1,2 +1,2 @@
-public class Datenelement {
+public abstract class Datenelement {
 }
--- src/Karte.java
+++ src/Karte.java
@@ -1,2 +1,5 @@
-public class Karte {
+public class Karte extends Datenelement {
+    String Farbe;
+    int Wert;
+    //0-9 Farbkarten; 20 = Aussetzen; 30 = 2+ Karte; 40 = 4+ Karte; 50 = normale Karte wünschen
 }
