cac5be84012e64fef282571310c23f17ca75992d
bstege <81328481+bstege@users.noreply.github.com>
2021-05-05 12:02:17 +0200

    Anfang kein Plan

--- src/Hauptmenu.java
+++ src/Hauptmenu.java
@@ -1,2 +1,19 @@
+// JMenuBar
+import java.awt.Color;
+import javax.swing.JDialog;
+import javax.swing.JMenuBar;
+import javax.swing.border.LineBorder;
+import javax.swing.border.Border;
+
 public class Hauptmenu {
+  
+  public Hauptmenu(){
+    JFrame hauptFrame = new JFrame ("Hauptframe");
+    hauptFrame.setSize(200;200);
+    hauptFrame.setVisible(true);
+  }
+  
+  public Liste liste = new Liste();
+  
+  
 }
