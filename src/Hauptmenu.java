895012d63a6c324060f0cc5cf700dd600319baf7
bstege <81328481+bstege@users.noreply.github.com>
2021-05-05 12:45:03 +0200

    Hauptmenü aber ohne funktionalität, nur Optik

--- src/Hauptmenu.java
+++ src/Hauptmenu.java
@@ -1,3 +1,5 @@
+// JDialog
+import javax.swing.JDialog;
 // JMenuBar
 import java.awt.Color;
 import javax.swing.JDialog;
@@ -8,12 +10,23 @@ import javax.swing.border.Border;
 public class Hauptmenu {
   
   public Hauptmenu(){
-    JFrame hauptFrame = new JFrame ("Hauptframe");
-    hauptFrame.setSize(200;200);
-    hauptFrame.setVisible(true);
+    JDialog hauptJDialog = new JDialog;
+    hauptJDialog.setTitel("Hauptmenü");
+    haputJDialog.setSize(450,300)
+      
+    // Border evtl
+    Border bo = new LineBorder(Color.yellow);
+    
+    JMenuBar bar = new MenuBar;
+    bar.setborder(bo);
+    JMenu menu = new JMenu("UNO");
+    JMenuItem item = new JMenuItem("Spielen");
+    menu.add(item);
+    bar.add(menu);
+    hauptJDialog.setJMenuBar(bar);
+    hauptJDialog.setVisible(true);
   }
   
-  public Liste liste = new Liste();
   
   
 }
